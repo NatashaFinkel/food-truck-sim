2f486135c322eecc719cea4198c4c7946af36b7e
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-03-21 07:44:58 +0100

    Feature : implémentation de showMenuItems() dans le dashBoard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,6 @@
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 import { addDishToTheMenu } from "../redux/slices/stockSlice";
 import mainDishes from "../food-data/main-dishes.json";
 import sideDishes from "../food-data/side-dishes.json";
@@ -6,6 +8,7 @@ import sideDishes from "../food-data/side-dishes.json";
 const Dashboard = () => {
   const dispatch = useDispatch();
   const combinedDishes = [...mainDishes, ...sideDishes];
+  const menuItems = useSelector((state: RootState) => state.stock.items);
 
   const handleAddDishToTheMenu = (dish: {
     id: string;
@@ -24,6 +27,33 @@ const Dashboard = () => {
   dispatch(addDishToTheMenu(newDish));
   };
 
+  function showMenuItems() {
+    const menuContainer = document.getElementById("menu-container");
+    const menu = JSON.parse(localStorage.getItem("Menu") || "[]");
+    if (menuContainer) {
+      console.log(menu);
+      menuContainer.innerHTML = "";
+      menu.forEach(
+        (dish: {
+          id: string;
+          name: string;
+          ingredients: string[];
+          price: number;
+          quantity: number;
+        }) => {
+          const dishElement = document.createElement("div");
+          dishElement.classList.add("menu-item");
+          dishElement.innerHTML = `<div>${dish.name} : ${dish.quantity}</div>`;
+          menuContainer.appendChild(dishElement);
+        }
+      );
+    }
+  }
+
+  useEffect(() => {
+    showMenuItems();
+  }, [menuItems]);
+
   return (
     <div className="dashboard">
       <h2>Y'a quoi à la carte, aujourd'hui ?</h2>
@@ -31,6 +61,7 @@ const Dashboard = () => {
         Choisissez jusqu'à 3 plats différents, ainsi que 2 accompagnements
         maximum.
       </p>
+      <div id="menu-container" className="menu-container"></div>
       <div className="ingredients-list">
         {combinedDishes.map((dish) => (
           <div key={dish.id} className="ingredients-list-item">
