fd9bdbacda1b1d633b331273526ca523c4c712e8
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-04-12 20:43:58 +0200

    Feature : implémentation de NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import NavBar from "./components/NavBar";
 import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>My little food truck ❤️</h1>
-      </header>
-      <Dashboard />
+      <NavBar />
+      <div style={{ marginLeft: "200px", padding: "20px" }}>
+        <header>
+          <h1>My little food truck ❤️</h1>
+        </header>
+        <Dashboard />
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar: React.FC = () => {
+  return (
+    <nav className="nav-bar">
+      <ul>
+        <li><a href="#dashboard">Dashboard</a></li>
+        <li><a href="#mission">Ma mission</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
