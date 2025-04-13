a55e5abdef458710fa568f6cd657ed4700b0a7d4
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-04-13 19:42:18 +0200

    Refacto : remplacement nom d'un fichier

--- src/TypeScript/getRandomIndex.ts
+++
@@ -1,6 +0,0 @@
-function getRandomIndex() {
-    const randomIndex: number = Math.floor(Math.random() * 100);
-    return randomIndex;
-}
-
-export default getRandomIndex;
---
+++ src/TypeScript/getRandomPopularityIndex.ts
@@ -0,0 +1,6 @@
+function getRandomPopularityIndex() {
+  const randomIndex: number = Math.floor(Math.random() * 100);
+  return randomIndex;
+}
+
+export default getRandomPopularityIndex;
