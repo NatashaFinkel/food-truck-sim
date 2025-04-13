f03d6fe324d45f31d250af93c0afe986dbfbfa33
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-04-13 16:48:44 +0200

    Feature : implémentation structure Mission.tsx avec Lorem Ipsum

---
+++ src/components/Mission.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Mission: React.FC = () => {
+  return (
+    <div className="dashboard">
+      <h2>Ta mission</h2>
+      <div className="mission-txt-div">
+        <p>
+          Et quia Mesopotamiae tractus omnes crebro inquietari sueti
+          praetenturis et stationibus servabantur agrariis, laevorsum flexo
+          itinere Osdroenae subsederat extimas partes, novum parumque aliquando
+          temptatum commentum adgressus. quod si impetrasset, fulminis modo
+          cuncta vastarat. erat autem quod cogitabat huius modi.
+        </p>
+
+        <p>
+          Saepissime igitur mihi de amicitia cogitanti maxime illud
+          considerandum videri solet, utrum propter imbecillitatem atque inopiam
+          desiderata sit amicitia, ut dandis recipiendisque meritis quod quisque
+          minus per se ipse posset, id acciperet ab alio vicissimque redderet,
+          an esset hoc quidem proprium amicitiae, sed antiquior et pulchrior et
+          magis a natura ipsa profecta alia causa.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Mission;
